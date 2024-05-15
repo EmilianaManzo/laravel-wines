bff191f564f09c0ad85a2dea068ef449543711c1
Oltion-Koceku <koceku17@gmail.com>
2024-05-15 16:32:22 +0200

    [add]controller

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -4,11 +4,16 @@ namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Wine;
 
 class PageController extends Controller
 {
     public function index(){
-        return view('wines');
+
+        $wines = Wine::all();
+
+
+        return view('wines', compact('wines'));
     }
 
 
