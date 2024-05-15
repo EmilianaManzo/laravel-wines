b732565a387e8a3959559dca684b50c084fdd7c9
EmilianaManzo <emiliana.ma96@gmail.com>
2024-05-15 16:06:36 +0200

    [change] Model

--- app/Models/wine.php
+++ app/Models/wine.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class wine extends Model
+class Wine extends Model
 {
     use HasFactory;
 }
