0c529e6cb236b7257e1ae9f04abaf7f00015ab59
Oltion-Koceku <koceku17@gmail.com>
2024-05-15 16:16:39 +0200

    [add]Controller and rout

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -4,14 +4,16 @@ namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Wines;
 
 class PageController extends Controller
 {
     public function index(){
-        return view('home');
+
+        $wines = Wines::all();
+
+        return view('wines', compact('wines'));
     }
 
-    public function nuovaPagina(){
-        return view('nuova-pagina');
-    }
+
 }
