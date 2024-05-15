55339f1a7a38f3e9dac21aa88594058035e170da
simoneaucello <simoneaucello@gmail.com>
2024-05-15 17:31:02 +0200

    [add] mod-scss

--- resources/views/wines.blade.php
+++ resources/views/wines.blade.php
@@ -1,26 +1,32 @@
 @extends('layout.main')
 
+@section('title')
+    Vini
+@endsection
+
 @section('content')
-    <h1 class="my-5 text-center text-bg-warning">Elenco vini</h1>
-    <div class="d-flex flex-wrap justify-content-around  ">
+    <div class="container my-5 my-cont">
+        <h1 class=" text-center text-warning">Elenco vini</h1>
+        <div class="d-flex flex-wrap justify-content-around  ">
 
-        @foreach ($wines as $item)
-            <div class="card my-3" style="width: 18rem;">
-                <img src="{{ $item->image }}" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title fw-bolder">{{ $item->wine }}</h5>
-                    <p class="card-text">{{ $item->winery }}</p>
+            @foreach ($wines as $item)
+                <div class="card my-3" style="width: 18rem;">
+                    <img src="{{ $item->image }}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title fw-bolder">{{ $item->wine }}</h5>
+                        <p class="card-text">{{ $item->winery }}</p>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">{{ $item->rating_average }}</li>
+                        <li class="list-group-item">{{ $item->rating_reviews }}</li>
+                        <li class="list-group-item">{{ $item->location }}</li>
+                    </ul>
+                    <div class="card-body">
+                        <a href="#" class="card-link bg-dark text-white fw-bolder">Scheda vino</a>
+                    </div>
                 </div>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">{{ $item->rating_average }}</li>
-                    <li class="list-group-item">{{ $item->rating_reviews }}</li>
-                    <li class="list-group-item">{{ $item->location }}</li>
-                </ul>
-                <div class="card-body">
-                    <a href="#" class="card-link bg-dark text-white fw-bolder">Scheda vino</a>
-                </div>
-            </div>
-        @endforeach
+            @endforeach
 
+        </div>
     </div>
 @endsection
